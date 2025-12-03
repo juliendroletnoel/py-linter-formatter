9492f460a9a25068ecee13a290776a1ff28dc8df
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-03 04:49:47 -0500

    Initial commit

--- app/main.py
+++ app/main.py
@@ -1,13 +1,19 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
-
+    formatted_dict = {}
+    formatted_dict["line"] = error["line_number"]
+    formatted_dict["column"] = error["column_number"]
+    formatted_dict["message"] = error["text"]
+    formatted_dict["name"] = error["code"]
+    formatted_dict["source"] = "flake8"
+    return formatted_dict
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
-
+    formatted_dict = []
+    formatted_dict["errors"] = [format_linter_error(error) for error in errors]
+    formatted_dict["path"] = file_path
+    formatted_dict["status"] = "failed"
+    return formatted_dict
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    formatted_list = []
+    return formatted_list
