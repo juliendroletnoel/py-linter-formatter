b8f86a28c2f208a09010f694b55013b8d83893d5
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-13 09:44:39 -0500

    Flake8

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ def format_linter_error(error: dict) -> dict:
     formatted_dict["source"] = "flake8"
     return formatted_dict
 
+
 def format_single_linter_file(file_path: str, errors: [dict]) -> dict:
     formatted_dict = []
     formatted_dict["errors"] = [format_linter_error(error) for error in errors]
@@ -19,14 +20,15 @@ def format_single_linter_file(file_path: str, errors: [dict]) -> dict:
 
     return formatted_dict
 
+
 def format_linter_report(linter_report: dict) -> list:
     new_format = {
-            "error": [],
-            "path": linter_report["path"],
-            "status": linter_report["status"]
-        }
+    "error": [],
+    "path": linter_report["path"],
+    "status": linter_report["status"]
+    }
 
     if new_format["status"] == "failed":
         new_format["error"] = [error for error in linter_report["errors"]]
-    
+
     return new_format
