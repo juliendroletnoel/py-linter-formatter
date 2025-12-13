bb76dbfe4ee9d49bbac0852989f431aa5f52e435
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-13 09:38:24 -0500

    Initial solution

--- app/main.py
+++ app/main.py
@@ -7,13 +7,26 @@ def format_linter_error(error: dict) -> dict:
     formatted_dict["source"] = "flake8"
     return formatted_dict
 
-def format_single_linter_file(file_path: str, errors: list) -> dict:
+def format_single_linter_file(file_path: str, errors: [dict]) -> dict:
     formatted_dict = []
     formatted_dict["errors"] = [format_linter_error(error) for error in errors]
     formatted_dict["path"] = file_path
-    formatted_dict["status"] = "failed"
+
+    status = "failed"
+    if len(formatted_dict["errors"]) == 0:
+        status = "success"
+    formatted_dict["status"] = status
+
     return formatted_dict
 
 def format_linter_report(linter_report: dict) -> list:
-    formatted_list = []
-    return formatted_list
+    new_format = {
+            "error": [],
+            "path": linter_report["path"],
+            "status": linter_report["status"]
+        }
+
+    if new_format["status"] == "failed":
+        new_format["error"] = [error for error in linter_report["errors"]]
+    
+    return new_format
