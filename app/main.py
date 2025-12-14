383266b424152ee6d80bafdf51a6c4ecb0374cf5
Julien Drolet-Noel <julien.drolet.noel@gmail.com>
2025-12-14 06:49:39 -0500

    Only one statement in format_linter_error

--- app/main.py
+++ app/main.py
@@ -1,11 +1,11 @@
 def format_linter_error(error: dict) -> dict:
-    formatted_dict = {}
-    formatted_dict["line"] = error["line_number"]
-    formatted_dict["column"] = error["column_number"]
-    formatted_dict["message"] = error["text"]
-    formatted_dict["name"] = error["code"]
-    formatted_dict["source"] = "flake8"
-    return formatted_dict
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: [dict]) -> dict:
